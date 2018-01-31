70539e8021c37851be73ed2d36bdbc3a65150cb9
Lambda <lambda_@outlook.com>
2018-01-30 22:06:55 -0500

    it works

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const telegraf = require('telegraf');
+
+const bot = new telegraf('');
+
+bot.start((ctx) =>
+{
+        ctx.telegram.sendMessage(ctx.message.chat.id, "Silencing...");
+});
+
+bot.on('message', (ctx) => 
+{
+        if ((ctx.message.document == undefined) ||  (ctx.message.document.mime_type !== "video/mp4"))
+                ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
+        //console.log("\n ", ctx.message.document);
+});
+
+bot.command('stop', bot.stop());
+bot.startPolling();
